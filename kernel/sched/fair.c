80c3cdf4540881cb7dc03a1b389f6058b6c807e6
friedrich420 <kelpidorou@yahoo.com>
2015-01-16 11:09:17 +0200

    Kernel/sched: Reduce Latency [Pafcholini]
    Signed-off-by: HolyAngel <slverwolf@gmail.com>
    Signed-off-by: Salllz <sal235222727@gmail.com>
    Signed-off-by: alanndz <alanndz7@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    (cherry picked from commit b857014598f975fd24ab06cca465ec47a7b0f3c9)
    Signed-off-by: Panchajanya1999 <kernel@panchajanya.dev>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -74,8 +74,8 @@ walt_dec_cfs_rq_stats(struct cfs_rq *cfs_rq, struct task_struct *p) {}
  *
  * (default: 6ms * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_latency			= 10000000ULL;
-unsigned int normalized_sysctl_sched_latency		= 10000000ULL;
+unsigned int sysctl_sched_latency			= 5000000ULL;
+unsigned int normalized_sysctl_sched_latency		= 5000000ULL;
 
 /*
  * Enable/disable honoring sync flag in energy-aware wakeups.
