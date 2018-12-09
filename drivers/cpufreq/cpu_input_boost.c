0861b11cac0bce84b44f8552224812484f336fe2
kdrag0n <dragon@khronodragon.com>
2018-12-09 11:35:50 -0800

    drm/drm_atomic: Boost only if last interaction was within 3s
    This should prevent AOD notification boosting and excessive boosting
    when watching videos.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: Subhajeet Muhuri <kenny3fcb@gmail.com>
    Signed-off-by: LibXZR <xzr467706992@163.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -82,6 +82,8 @@ struct boost_drv {
 	unsigned long state;
 };
 
+unsigned long last_input_time;
+
 static void input_unboost_worker(struct work_struct *work);
 static void max_unboost_worker(struct work_struct *work);
 
@@ -325,6 +327,8 @@ static void cpu_input_boost_input_event(struct input_handle *handle,
 	struct boost_drv *b = handle->handler->private;
 
 	__cpu_input_boost_kick(b);
+
+	last_input_time = jiffies;
 }
 
 static int cpu_input_boost_input_connect(struct input_handler *handler,
--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -2601,8 +2601,8 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 			(arg->flags & DRM_MODE_PAGE_FLIP_EVENT))
 		return -EINVAL;
 
-	/* Boost DDR and CPU when committing a new frame */
-	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
+	/* Boost CPU and DDR when committing a new frame */
+	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY) && time_before(jiffies, last_input_time + msecs_to_jiffies(3000))) {
 		devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
 		if (sysctl_sched_boost)
 			cpu_input_boost_kick();
