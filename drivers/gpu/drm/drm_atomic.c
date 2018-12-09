3d6bf5b6b22019ce8ff550e2e0747273d8195575
kdrag0n <dragon@khronodragon.com>
2018-12-09 11:35:50 -0800

    drm/drm_atomic: Boost only if last interaction was within 3s
    This should prevent AOD notification boosting and excessive boosting
    when watching videos.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: Subhajeet Muhuri <kenny3fcb@gmail.com>
    Signed-off-by: LibXZR <xzr467706992@163.com>
    Change-Id: Ia9ae30a539fc5b2f7e6753cd01d0ca17fdc1a967

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -2602,7 +2602,7 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 		return -EINVAL;
 
 	/* Boost CPU and DDR when committing a new frame */
-	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
+	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY) && time_before(jiffies, last_input_time + msecs_to_jiffies(3000))) {
 		devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
 		if (sysctl_sched_boost)
 			cpu_input_boost_kick();
