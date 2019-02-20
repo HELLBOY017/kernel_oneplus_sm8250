d7a3b5d1236d65a4967557a269422fa2815ee8f3
freak07 <michalechner92@googlemail.com>
2019-02-20 10:22:43 +0100

    techpack: use power efficient workingqueues
    (cherry picked from commit 75ed04dc1f7e7da565e41b7190fb44f078c5b4d6)
    (cherry picked from commit a23fc1d9e2a6166dde561c6e5cf8f1e09e0a5c0c)
    (cherry picked from commit efed5ad0c48e83e66b4be7f33147aa192e47e498)
    (cherry picked from commit 8a55be718c836abf6d4bc5a1233e982374fd3316)
    (cherry picked from commit b6c2999fdb6f3d71385d1a1edf839ff14e4bc0d7)
    (cherry picked from commit 34715e51bf1fd10b6fec2e581524b20286fb46c4)
    (cherry picked from commit 5fec4357e73939eb0a6252f7848480d8bf624fe6)
    (cherry picked from commit 7d6be584cf1ea2879fd8ef21178766b06be13fe8)
    (cherry picked from commit 8b0aa1ebc13c6ccd9a250bdf5004d64da2f089aa)
    (cherry picked from commit 1485ebd685e6d2af1b45ab43297b9d32ee0aacc2)
    (cherry picked from commit 089735f8cf8b4808bc52901da2b337b733983f4c)

--- techpack/audio/asoc/codecs/sdm660_cdc/msm-digital-cdc.c
+++ techpack/audio/asoc/codecs/sdm660_cdc/msm-digital-cdc.c
@@ -101,7 +101,7 @@ static int msm_digcdc_clock_control(bool flag)
 			}
 			pr_debug("enabled digital codec core clk\n");
 			atomic_set(&pdata->int_mclk0_enabled, true);
-			schedule_delayed_work(&pdata->disable_int_mclk0_work,
+			queue_delayed_work(system_power_efficient_wq,&pdata->disable_int_mclk0_work,
 					      50);
 		}
 	} else {
@@ -991,13 +991,13 @@ static int msm_dig_cdc_codec_enable_dec(struct snd_soc_dapm_widget *w,
 		snd_soc_component_update_bits(component, tx_mux_ctl_reg,
 						0x08, 0x00);
 
-		schedule_delayed_work(
+		queue_delayed_work(system_power_efficient_wq,
 			    &msm_dig_cdc->tx_mute_dwork[decimator - 1].dwork,
 			    msecs_to_jiffies(tx_unmute_delay));
 		if (tx_hpf_work[decimator - 1].tx_hpf_cut_of_freq !=
 				CF_MIN_3DB_150HZ) {
 
-			schedule_delayed_work(&tx_hpf_work[decimator - 1].dwork,
+			queue_delayed_work(system_power_efficient_wq, &tx_hpf_work[decimator - 1].dwork,
 					msecs_to_jiffies(300));
 		}
 		/* apply the digital gain after the decimator is enabled*/
