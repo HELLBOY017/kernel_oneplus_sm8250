d0f38af202b315698051f8d2f135ed32fd3088b6
freak07 <michalechner92@googlemail.com>
2019-02-20 10:22:43 +0100

    techpack: use power efficient workingqueues
    (cherry picked from commit 75ed04dc1f7e7da565e41b7190fb44f078c5b4d6)
    (cherry picked from commit a23fc1d9e2a6166dde561c6e5cf8f1e09e0a5c0c)
    (cherry picked from commit efed5ad0c48e83e66b4be7f33147aa192e47e498)
    (cherry picked from commit 8a55be718c836abf6d4bc5a1233e982374fd3316)
    (cherry picked from commit b6c2999fdb6f3d71385d1a1edf839ff14e4bc0d7)
    (cherry picked from commit 34715e51bf1fd10b6fec2e581524b20286fb46c4)
    (cherry picked from commit 5fec4357e73939eb0a6252f7848480d8bf624fe6)
    (cherry picked from commit 7d6be584cf1ea2879fd8ef21178766b06be13fe8)
    (cherry picked from commit 8b0aa1ebc13c6ccd9a250bdf5004d64da2f089aa)
    (cherry picked from commit 1485ebd685e6d2af1b45ab43297b9d32ee0aacc2)
    (cherry picked from commit 089735f8cf8b4808bc52901da2b337b733983f4c)

--- techpack/audio/asoc/codecs/wsa881x.c
+++ techpack/audio/asoc/codecs/wsa881x.c
@@ -1049,7 +1049,7 @@ static void wsa881x_ocp_ctl_work(struct work_struct *work)
 		snd_soc_component_update_bits(component, WSA881X_SPKR_OCP_CTL,
 					0xC0, 0xC0);
 
-	schedule_delayed_work(&wsa881x->ocp_ctl_work,
+	queue_delayed_work(system_power_efficient_wq, &wsa881x->ocp_ctl_work,
 			msecs_to_jiffies(wsa881x_ocp_poll_timer_sec * 1000));
 }
 
@@ -1114,7 +1114,7 @@ static int wsa881x_spkr_pa_event(struct snd_soc_dapm_widget *w,
 						0x07, 0x01);
 			wsa881x_visense_adc_ctrl(component, ENABLE);
 		}
-		schedule_delayed_work(&wsa881x->ocp_ctl_work,
+		queue_delayed_work(system_power_efficient_wq, &wsa881x->ocp_ctl_work,
 			msecs_to_jiffies(WSA881X_OCP_CTL_TIMER_SEC * 1000));
 		/* Force remove group */
 		swr_remove_from_group(wsa881x->swr_slave,
