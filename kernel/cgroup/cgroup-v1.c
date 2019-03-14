514fae3a43f8bfdf8897231e5796189f6629ec02
Sultan Alsawaf <sultan@kerneltoast.com>
2019-03-13 22:41:46 -0700

    kernel: Boost whenever a zygote-forked process becomes a top app
    Boost the CPU & CPUBW to the max for 500 ms whenever the top app changes, which
    improves app launch speeds and addresses jitter when switching between
    apps. A check to make sure that the top-app's parent is zygote ensures
    that a user-facing app is indeed what's added to the top app task group,
    since app processes are forked from zygote.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [0ctobot: Adapt for 4.19]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- kernel/cgroup/cgroup-v1.c
+++ kernel/cgroup/cgroup-v1.c
@@ -13,6 +13,8 @@
 #include <linux/delayacct.h>
 #include <linux/pid_namespace.h>
 #include <linux/cgroupstats.h>
+#include <linux/binfmts.h>
+#include <linux/cpu_input_boost.h>
 
 #include <trace/events/cgroup.h>
 
@@ -537,6 +539,13 @@ static ssize_t __cgroup1_procs_write(struct kernfs_open_file *of,
 		goto out_finish;
 
 	ret = cgroup_attach_task(cgrp, task, threadgroup);
+
+	/* This covers boosting for app launches and app transitions */
+	if (!ret && !threadgroup &&
+		!memcmp(of->kn->parent->name, "top-app", sizeof("top-app")) &&
+		is_zygote_pid(task->parent->pid)) {
+		cpu_input_boost_kick_max(1000);
+	}	
 
 out_finish:
 	cgroup_procs_write_finish(task);
