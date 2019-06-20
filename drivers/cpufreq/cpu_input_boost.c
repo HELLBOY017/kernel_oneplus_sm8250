03bf494756fc2b3c680d6917d3116290f4f21419
Yaroslav Furman <yaro330@gmail.com>
2019-06-20 16:51:50 +0300

    cpu_input_boost: add support for boost freqs lower than min freq
    Adds ability to disable boosting of certain clusters by
    setting boost freq to 0 for it. Otherwise freq would
    drop to cpuinfo.min, which isn't desirable.
    
    [@0ctobot: Extend to cover max_boost_freq]
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -98,11 +98,11 @@ static unsigned int get_input_boost_freq(struct cpufreq_policy *policy)
 	unsigned int freq;
 
 	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
-		freq = input_boost_freq_little;
+		freq = max(input_boost_freq_little, cpu_freq_min_little);
 	else if (cpumask_test_cpu(policy->cpu, cpu_perf_mask))
-		freq = input_boost_freq_big;
+		freq = max(input_boost_freq_big, cpu_freq_min_big);
 	else
-		freq = input_boost_freq_prime;
+		freq = max(input_boost_freq_prime, cpu_freq_min_prime);
 	return min(freq, policy->max);
 }
 
@@ -111,11 +111,11 @@ static unsigned int get_max_boost_freq(struct cpufreq_policy *policy)
 	unsigned int freq;
 
 	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
-		freq = max_boost_freq_little;
+		freq = max(max_boost_freq_little, cpu_freq_min_little);
 	else if (cpumask_test_cpu(policy->cpu, cpu_perf_mask))
-		freq = max_boost_freq_big;
+		freq = max(max_boost_freq_big, cpu_freq_min_big);
 	else
-		freq = max_boost_freq_prime;
+		freq = max(max_boost_freq_prime, cpu_freq_min_prime);
 	return min(freq, policy->max);
 }
 
