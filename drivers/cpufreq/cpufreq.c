f5ef088d20567f4c46a21bf17285f1d8e5ba8a00
Danny Lin <danny@kdrag0n.dev>
2019-08-04 07:39:15 +0000

    cpufreq: Only block min freq setting for booster processes
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -17,6 +17,7 @@
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
+#include <linux/binfmts.h>
 #include <linux/cpu.h>
 #include <linux/cpufreq.h>
 #include <linux/cpufreq_times.h>
@@ -727,7 +728,8 @@ static ssize_t store_##file_name					\
 	int ret, temp;							\
 	struct cpufreq_policy new_policy;				\
 									\
-	if (&policy->object == &policy->min)				\
+	if (&policy->object == &policy->min &&				\
+	    task_is_booster(current))					\
 		return count;						\
 									\
 	memcpy(&new_policy, policy, sizeof(*policy));			\
