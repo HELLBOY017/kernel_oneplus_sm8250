034c8243e759017e4e21f86953bbdd89157ea881
Park Ju Hyung <qkrwngud825@gmail.com>
2020-06-16 18:12:33 +0200

    power: reset: msm: Disable download mode
    Disable it for better pstore logging.
    
    Change-Id: Ia6be1cff22c4117c0e29dbecf38e769482baea99

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -62,7 +62,7 @@ static void scm_disable_sdi(void);
  * There is no API from TZ to re-enable the registers.
  * So the SDI cannot be re-enabled when it already by-passed.
  */
-static int download_mode = 1;
+static int download_mode = 0;
 static struct kobject dload_kobj;
 
 static int in_panic;
