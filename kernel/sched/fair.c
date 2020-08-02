36789db80c2f6d1082ba74770acfda133c808f2a
Rick Yiu <rickyiu@google.com>
2020-08-02 12:12:34 +0800

    sched/fair: fix can_migrate_boosted_task logic
    Original logic will prevent task that prefer high cap and prio <=
    DEFAULT_PRIO to migrate within little cluster. It might cause
    problem for specific load pattern. Also refined some code flow.
    
    Bug: 162300017
    Test: ITS pass
    Change-Id: Id8ebc21480a2a1caa8ad26233a05d0410c7588e8
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    (cherry picked from commit 6a51da02bd999667e44f1cd8c5c9b84f14dfa5ad)

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -2846,8 +2846,16 @@ static bool bitmap_testbit(unsigned long *map, unsigned long bit)
 
 static void inc_prioritized_task_count(struct rq *rq, struct task_struct *p)
 {
-	bool bitset = bitmap_testbit(per_cpu(prioritized_task_mask, cpu_of(rq)),
-				     p->pid);
+	bool bitset;
+
+	if (is_min_capacity_cpu(cpu_of(rq)))
+		return;
+
+	if (p->pid > PID_MAX_DEFAULT)
+		return;
+
+	bitset = bitmap_testbit(per_cpu(prioritized_task_mask, cpu_of(rq)),
+				p->pid);
 
 	if (schedtune_prefer_high_cap(p) && p->prio <= DEFAULT_PRIO) {
 		if (likely(!bitset)) {
@@ -2865,6 +2873,12 @@ static void inc_prioritized_task_count(struct rq *rq, struct task_struct *p)
 
 static void dec_prioritized_task_count(struct rq *rq, struct task_struct *p)
 {
+	if (is_min_capacity_cpu(cpu_of(rq)))
+		return;
+
+	if (p->pid > PID_MAX_DEFAULT)
+		return;
+
 	if (bitmap_testbit(per_cpu(prioritized_task_mask, cpu_of(rq)),
 			   p->pid)) {
 		__bitmap_clear(per_cpu(prioritized_task_mask, cpu_of(rq)),
@@ -9001,7 +9015,7 @@ static inline bool can_migrate_boosted_task(struct task_struct *p,
 	     task_in_related_thread_group(p) &&
 	     (capacity_orig_of(dst_cpu) < capacity_orig_of(src_cpu))) ||
 	    (schedtune_prefer_high_cap(p) && p->prio <= DEFAULT_PRIO &&
-	     is_min_capacity_cpu(dst_cpu)))
+	     !is_min_capacity_cpu(src_cpu) && is_min_capacity_cpu(dst_cpu)))
 		return false;
 	return true;
 }
