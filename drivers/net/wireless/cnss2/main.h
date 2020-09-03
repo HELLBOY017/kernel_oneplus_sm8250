b3ba70be8f5b046101f85711f53cd732efa1b3b5
Sandeep Singh <sandsing@codeaurora.org>
2020-09-03 10:44:14 -0700

    cnss2: Setup DMS QMI client for WLAN MAC address
    WLAN MAC address can be provisioned using NV memory based setup.
    DMS QMI server connection is required to retrieve this MAC address.
    Thus setup DMS QMI client in cnss platform driver and send this
    MAC address for WLAN FW before mission mode enable.
    
    Change-Id: I4dac4c589910a34952b0f63fd4394f5dd02315b8
    Signed-off-by: Sandeep Singh <sandsing@codeaurora.org>
    Signed-off-by: Manikandan Mohan <manikand@codeaurora.org>

--- drivers/net/wireless/cnss2/main.h
+++ drivers/net/wireless/cnss2/main.h
@@ -220,6 +220,8 @@ enum cnss_driver_event_type {
 	CNSS_DRIVER_EVENT_QDSS_TRACE_REQ_MEM,
 	CNSS_DRIVER_EVENT_QDSS_TRACE_SAVE,
 	CNSS_DRIVER_EVENT_QDSS_TRACE_FREE,
+	CNSS_DRIVER_EVENT_DMS_SERVER_ARRIVE,
+	CNSS_DRIVER_EVENT_DMS_SERVER_EXIT,
 	CNSS_DRIVER_EVENT_MAX,
 };
 
@@ -242,6 +244,7 @@ enum cnss_driver_state {
 	CNSS_IN_SUSPEND_RESUME,
 	CNSS_IN_REBOOT,
 	CNSS_QMI_DEL_SERVER,
+	CNSS_QMI_DMS_CONNECTED = 20,
 };
 
 struct cnss_recovery_data {
@@ -342,6 +345,11 @@ enum cnss_timeout_type {
 	CNSS_TIMEOUT_RECOVERY,
 };
 
+struct cnss_dms_data {
+	u32 mac_valid;
+	u8 mac[QMI_WLFW_MAC_ADDR_SIZE_V01];
+};
+
 struct cnss_plat_data {
 	struct platform_device *plat_dev;
 	void *bus_priv;
@@ -371,6 +379,7 @@ struct cnss_plat_data {
 	struct workqueue_struct *event_wq;
 	struct work_struct recovery_work;
 	struct qmi_handle qmi_wlfw;
+	struct qmi_handle qmi_dms;
 	struct wlfw_rf_chip_info chip_info;
 	struct wlfw_rf_board_info board_info;
 	struct wlfw_soc_info soc_info;
@@ -419,7 +428,7 @@ struct cnss_plat_data {
 	u8 fw_pcie_gen_switch;
 	u8 pcie_gen_speed;
 	int power_up_error;
-
+	struct cnss_dms_data dms;
     #ifdef OPLUS_FEATURE_WIFI_DCS_SWITCH
     //Add for wifi switch monitor
 	unsigned long loadBdfState;
