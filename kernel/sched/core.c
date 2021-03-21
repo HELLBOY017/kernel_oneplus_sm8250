edaf3492251e03161124cb1e90527293184f19e2
Sultan Alsawaf <sultan@kerneltoast.com>
2021-03-21 00:06:54 -0700

    sched/core: Skip rq lock in try_to_wake_up() when WALT is disabled
    When WALT is disabled, do_pl_notif() always returns false, in which case
    this bit of code serves no purpose. As a result, this WALT-specific code
    spins on acquiring the rq lock in a hot path, wasting CPU time. Compile
    it out when WALT is disabled to eliminate the unnecessary overhead.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Change-Id: Iebc4748e78c6bfd28f989cc052bedcfc87e31412

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2823,6 +2823,7 @@ stat:
 out:
 	raw_spin_unlock_irqrestore(&p->pi_lock, flags);
 
+#ifdef CONFIG_SCHED_WALT
 	if (success && sched_predl) {
 		raw_spin_lock_irqsave(&cpu_rq(cpu)->lock, flags);
 		if (do_pl_notif(cpu_rq(cpu)))
@@ -2831,6 +2832,7 @@ out:
 						SCHED_CPUFREQ_PL);
 		raw_spin_unlock_irqrestore(&cpu_rq(cpu)->lock, flags);
 	}
+#endif
 	return success;
 }
 
