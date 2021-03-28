764946af6611967bc944457105d298169f81f154
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2021-03-28 15:07:02 -0400

    techpack: display: Boost CPU and DDR to the max during fingerprint scans
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>
    [0ctobot: Squash idkwhoiam322/weeb_kernel_oneplus_sm8150@1f0ecc2 with
    idkwhoiam322/weeb_kernel_oneplus_sm8150@a5f71e4. Adapt for 4.19]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Change-Id: I67ef3ac1dee62efb63acc43df57121757fd5dc2b

--- techpack/display/msm/sde/sde_crtc.c
+++ techpack/display/msm/sde/sde_crtc.c
@@ -28,6 +28,9 @@
 #include <drm/drm_flip_work.h>
 #include <linux/clk/qcom.h>
 
+#include <linux/cpu_input_boost.h>
+#include <linux/devfreq_boost.h>
+
 #include "sde_kms.h"
 #include "sde_hw_lm.h"
 #include "sde_hw_ctl.h"
@@ -4745,6 +4748,12 @@ static int sde_crtc_onscreenfinger_atomic_check(struct sde_crtc_state *cstate,
 		if (pstates[i].sde_pstate)
 			pstates[i].sde_pstate->is_skip = false;
 	}
+
+	if (fppressed_index > 0 || fp_mode == 1) {
+                cpu_input_boost_kick_max(500);
+                devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 500);
+                devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 500);
+        }
 
 	if (!is_dsi_panel(cstate->base.crtc))
 		return 0;
