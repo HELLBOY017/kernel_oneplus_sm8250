85d7a195bc40b809b8b34e084067cffcad9610f5
Adam W. Willis <return.of.octobot@gmail.com>
2021-04-11 08:08:05 -0400

    Revert "sched: fair: Always try to use energy efficient cpu for wakeups"
    This partially reverts commit 39d60ed915f6f8aca2a2fa59d1f890dc7e38b762.
    
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Change-Id: I1be1d8836758b013b965e63e1b15e1d1f27afac6

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8008,16 +8008,6 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 	int want_affine = 0;
 	int sync = (wake_flags & WF_SYNC) && !(current->flags & PF_EXITING);
 
-	if (static_branch_unlikely(&sched_energy_present)) {
-		rcu_read_lock();
-		new_cpu = find_energy_efficient_cpu(p, prev_cpu, sync,
-						    sibling_count_hint);
-		if (unlikely(new_cpu < 0))
-			new_cpu = prev_cpu;
-		rcu_read_unlock();
-		return new_cpu;
-	}
-
 	if (sd_flag & SD_BALANCE_WAKE) {
 		int _wake_cap = wake_cap(p, cpu, prev_cpu);
 		int _cpus_allowed = cpumask_test_cpu(cpu, &p->cpus_allowed);
