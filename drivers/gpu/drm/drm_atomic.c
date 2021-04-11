117849c1a35182680d1df55b873a2bd34e713933
Adam W. Willis <return.of.octobot@gmail.com>
2021-04-11 16:39:28 -0400

    sched/boost: Boost DDR bus when sched_boost is triggered
    Suggested-by: Yaroslav Furman <yaro330@gmail.com>
    Co-authored-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>
    [@0ctobot: Squash idkwhoiam322/weeb_kernel_oneplus_sm8150@652b6ae
    with idkwhoiam322/weeb_kernel_oneplus_sm8150@2289c62 and
    idkwhoiam322/weeb_kernel_oneplus_sm8150@65ce97e]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -33,6 +33,8 @@
 #include <drm/drm_writeback.h>
 #include <linux/sync_file.h>
 #include <linux/pm_qos.h>
+#include <linux/devfreq_boost.h>
+#include <linux/sched/sysctl.h>
 
 #include "drm_crtc_internal.h"
 #include "drm_internal.h"
@@ -2597,6 +2599,13 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 	if ((arg->flags & DRM_MODE_ATOMIC_TEST_ONLY) &&
 			(arg->flags & DRM_MODE_PAGE_FLIP_EVENT))
 		return -EINVAL;
+
+	/* Boost DDR when committing a new frame if sched_boost > 0 */
+	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY) &&
+			sysctl_sched_boost) {
+		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+		devfreq_boost_kick(DEVFREQ_MSM_LLCCBW);
+	}
 
 	drm_modeset_acquire_init(&ctx, DRM_MODESET_ACQUIRE_INTERRUPTIBLE);
 
