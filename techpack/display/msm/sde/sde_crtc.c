e64d01be0d7fe18de66c3c0340de80b56b72066f
Sultan Alsawaf <sultan@kerneltoast.com>
2021-07-04 22:35:09 -0400

    [FIXUP] c4c999c ("devfreq_boost: Introduce devfreq boost driver")
    This driver boosts enumerated devfreq devices upon input, and allows for
    boosting specific devfreq devices on other custom events. The boost
    frequencies for this driver should be set so that frame drops are
    near-zero at the boosted frequencies and power consumption is minimized
    at said frequencies. The goal of this driver is to provide an interface
    to achieve optimal device performance by requesting boosts on key
    events, such as when a frame is ready to rendered to the display.
    
    Currently, support is only present for boosting the cpubw devfreq
    device, but the driver is structured in a way that makes it easy to add
    support for new boostable devfreq devices in the future.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [@0ctobot: Bring inline with kerneltoast/android_kernel_google_floral@dce5aa5,
    amending the following commits where applicable:
    076de0a ("[2/2] devfreq_boost: Rework display state awareness logic for OnePlus 8 devices")
    bf07438 ("devfreq_boost: Update and expand to handle CPUBW/LLCCBW boosting")
    2ca6410 ("devfreq_boost: Mark boost kthreads as performance critical")]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- techpack/display/msm/sde/sde_crtc.c
+++ techpack/display/msm/sde/sde_crtc.c
@@ -4763,8 +4763,8 @@ static int sde_crtc_onscreenfinger_atomic_check(struct sde_crtc_state *cstate,
 
 	if (fppressed_index > 0 || fp_mode == 1) {
 		cpu_input_boost_kick_max(500);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 500);
-		devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 500);
+		devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 500);
+		devfreq_boost_kick_max(DEVFREQ_CPU_CPU_LLCC_BW, 500);
 	}
 
 	if (oplus_dimlayer_bl_enable) {
