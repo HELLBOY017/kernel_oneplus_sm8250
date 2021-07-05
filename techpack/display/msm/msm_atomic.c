99a5e8ff7a178d00ef9d51ec6b822aee36286c5f
Sultan Alsawaf <sultan@kerneltoast.com>
2021-07-04 21:06:03 -0400

    drm/msm: Reduce latency while completing non-blocking commits
    In order to complete a commit, we must first wait for the previous
    commit to finish up. This is done by sleeping, during which time the CPU
    can enter a deep idle state and take a while to finish processing the
    commit after the wait is over. We can alleviate this by optimistically
    assuming that the kthread this commit worker is running on won't migrate
    to another CPU partway through. We only do this for the non-blocking
    case where the commit completion is done in an asynchronous worker
    because the generic DRM code already does this for atomic ioctls. Since
    non-blocking commits are the common case, preemptively kick the prime
    CPU out of idle when the atomic ioctl runs so that it'll be ready to
    process the DRM IRQ and display commit kthreads.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [@0ctobot: Adapt for 4.19]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- techpack/display/msm/msm_atomic.c
+++ techpack/display/msm/msm_atomic.c
@@ -20,6 +20,7 @@
 #include <linux/msm_drm_notify.h>
 #include <linux/notifier.h>
 #endif /* OPLUS_BUG_STABILITY */
+#include <linux/pm_qos.h>
 
 #include "msm_drv.h"
 #include "msm_gem.h"
@@ -594,10 +595,21 @@ static void _msm_drm_commit_work_cb(struct kthread_work *work)
 	}
 
 	commit = container_of(work, struct msm_commit, commit_work);
+	struct pm_qos_request req = {
+		.type = PM_QOS_REQ_AFFINE_CORES,
+		.cpus_affine = ATOMIC_INIT(BIT(raw_smp_processor_id()))
+	};
 
+	/*
+	 * Optimistically assume the current task won't migrate to another CPU
+	 * and restrict the current CPU to shallow idle states so that it won't
+	 * take too long to resume after waiting for the prior commit to finish.
+	 */
+	pm_qos_add_request(&req, PM_QOS_CPU_DMA_LATENCY, 100);
 	SDE_ATRACE_BEGIN("complete_commit");
 	complete_commit(commit);
 	SDE_ATRACE_END("complete_commit");
+	pm_qos_remove_request(&req);
 }
 
 static struct msm_commit *commit_init(struct drm_atomic_state *state,
