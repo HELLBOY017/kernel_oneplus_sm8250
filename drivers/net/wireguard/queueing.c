cff7dfbd34641c74c4c8d436874bc7a959e09f71
Jason A. Donenfeld <Jason@zx2c4.com>
2021-11-29 10:39:26 -0500

    UPSTREAM: wireguard: receive: use ring buffer for incoming handshakes
    Apparently the spinlock on incoming_handshake's skb_queue is highly
    contended, and a torrent of handshake or cookie packets can bring the
    data plane to its knees, simply by virtue of enqueueing the handshake
    packets to be processed asynchronously. So, we try switching this to a
    ring buffer to hopefully have less lock contention. This alleviates the
    problem somewhat, though it still isn't perfect, so future patches will
    have to improve this further. However, it at least doesn't completely
    diminish the data plane.
    
    Reported-by: Streun Fabio <fstreun@student.ethz.ch>
    Reported-by: Joel Wanner <joel.wanner@inf.ethz.ch>
    Fixes: e7096c131e51 ("net: WireGuard secure network tunnel")
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Change-Id: Ia4c756050837c7a47f01a065063baf0dd4844141

--- drivers/net/wireguard/queueing.c
+++ drivers/net/wireguard/queueing.c
@@ -46,10 +46,9 @@ int wg_packet_queue_init(struct crypt_queue *queue, work_func_t function,
 	return 0;
 }
 
-void wg_packet_queue_free(struct crypt_queue *queue, bool multicore)
+void wg_packet_queue_free(struct crypt_queue *queue, bool purge)
 {
-	if (multicore)
-		free_percpu(queue->worker);
-	WARN_ON(!__ptr_ring_empty(&queue->ring));
-	ptr_ring_cleanup(&queue->ring, NULL);
+	free_percpu(queue->worker);
+	WARN_ON(!purge && !__ptr_ring_empty(&queue->ring));
+	ptr_ring_cleanup(&queue->ring, purge ? (void(*)(void*))kfree_skb : NULL);
 }
