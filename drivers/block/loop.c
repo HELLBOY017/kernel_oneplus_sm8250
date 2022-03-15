41e8e81f0576fcc9ae884c422082266110de22b1
John Galt <johngaltfirstrun@gmail.com>
2022-03-15 11:39:40 -0400

    treewide: move some perf/prime affines to hp affine
    It was found poor irq balancing behavior from these changes can actually
    increase latency. Allow sane balancing for these kthreads/irq.
    
    DRM was kept on prime because balancing has negative impacts.
    pp_event was moved to hp because it's quite heavy and not inherently
    latency sensitive.

--- drivers/block/loop.c
+++ drivers/block/loop.c
@@ -939,8 +939,9 @@ static int loop_kthread_worker_fn(void *worker_ptr)
 static int loop_prepare_queue(struct loop_device *lo)
 {
 	kthread_init_worker(&lo->worker);
-	lo->worker_task = kthread_run(loop_kthread_worker_fn,
-			&lo->worker, "loop%d", lo->lo_number);
+	lo->worker_task = kthread_run_perf_critical(cpu_hp_mask,
+			loop_kthread_worker_fn, &lo->worker,
+			"loop%d", lo->lo_number);
 	if (IS_ERR(lo->worker_task))
 		return -ENOMEM;
 	set_user_nice(lo->worker_task, MIN_NICE);
