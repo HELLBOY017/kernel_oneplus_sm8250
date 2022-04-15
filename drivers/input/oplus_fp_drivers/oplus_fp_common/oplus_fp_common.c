bd5766c8c21f6ff95c7cbf674fa19c52d67a7b6c
LuK1337 <priv.luk@gmail.com>
2022-04-15 10:54:44 +0200

    display: oplus: Add fp_state sysfs
    This lets us to poll it in userspace.
    
    Change-Id: I999d165b3b872475d793a60f5fac5e5d97355e11

--- drivers/input/oplus_fp_drivers/oplus_fp_common/oplus_fp_common.c
+++ drivers/input/oplus_fp_drivers/oplus_fp_common/oplus_fp_common.c
@@ -56,7 +56,7 @@ static struct fp_data *fp_data_ptr = NULL;
 char g_engineermode_menu_config[ENGINEER_MENU_SELECT_MAXLENTH] = ENGINEER_MENU_DEFAULT;
 
 static DEFINE_MUTEX(opticalfp_handler_lock);
-static opticalfp_handler g_opticalfp_irq_handler = NULL;
+static opticalfp_handler g_opticalfp_irq_handlers[5 /* lazy */] = {0};
 
 
 static int get_manufacture_id_value(struct fp_data *fp_data)
@@ -249,9 +249,15 @@ static struct file_operations fp_id_node_ctrl = {
 };
 
 void opticalfp_irq_handler_register(opticalfp_handler handler) {
+    int i;
     if (handler) {
         mutex_lock(&opticalfp_handler_lock);
-        g_opticalfp_irq_handler = handler;
+        for (i = 0; i < ARRAY_SIZE(g_opticalfp_irq_handlers); i++) {
+            if (!g_opticalfp_irq_handlers[i]) {
+                g_opticalfp_irq_handlers[i] = handler;
+                break;
+            }
+        }
         mutex_unlock(&opticalfp_handler_lock);
     } else {
         pr_err("%s handler is NULL", __func__);
@@ -259,11 +265,12 @@ void opticalfp_irq_handler_register(opticalfp_handler handler) {
 }
 
 int opticalfp_irq_handler(struct fp_underscreen_info* tp_info) {
-    if (g_opticalfp_irq_handler) {
-        return g_opticalfp_irq_handler(tp_info);
-    } else {
-        return FP_UNKNOWN;
+    int i, ret = FP_UNKNOWN;
+    for (i = 0; i < ARRAY_SIZE(g_opticalfp_irq_handlers); i++) {
+        if (g_opticalfp_irq_handlers[i])
+            ret = g_opticalfp_irq_handlers[i](tp_info);
     }
+    return ret;
 }
 EXPORT_SYMBOL(opticalfp_irq_handler);
 
