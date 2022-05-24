4a5fe0f74776de448d874fd60fe340b79e98cb2a
Juhyung Park <qkrwngud825@gmail.com>
2022-05-24 22:22:07 +0900

    mm: multi-gen LRU: export min_ttl unsatisfied counter to sysfs
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Change-Id: Ief4fab6bb5d43dffaf90652960e0957e6688b872

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -3910,6 +3910,7 @@ static bool age_lruvec(struct lruvec *lruvec, struct scan_control *sc,
 
 /* to protect the working set of the last N jiffies */
 static unsigned long lru_gen_min_ttl __read_mostly;
+static int lru_gen_min_ttl_unsatisfied;
 
 static void lru_gen_age_node(struct pglist_data *pgdat, struct scan_control *sc)
 {
@@ -3956,6 +3957,7 @@ static void lru_gen_age_node(struct pglist_data *pgdat, struct scan_control *sc)
 	 */
 	if (!success) {
 		pr_err("mglru: min_ttl unsatisfied, calling OOM killer\n");
+		lru_gen_min_ttl_unsatisfied++;
 		if (mutex_trylock(&oom_lock)) {
 			struct oom_control oc = {
 				.gfp_mask = sc->gfp_mask,
@@ -4695,6 +4697,15 @@ static struct kobj_attribute lru_gen_min_ttl_attr = __ATTR(
 	min_ttl_ms, 0644, show_min_ttl, store_min_ttl
 );
 
+static ssize_t show_min_ttl_unsatisfied(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+	return sprintf(buf, "%d\n", lru_gen_min_ttl_unsatisfied);
+}
+
+static struct kobj_attribute lru_gen_min_ttl_unsatisfied_attr = __ATTR(
+	min_ttl_unsatisfied, 0444, show_min_ttl_unsatisfied, NULL
+);
+
 static ssize_t show_enable(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
 {
 	unsigned int caps = 0;
@@ -4743,6 +4754,7 @@ static struct kobj_attribute lru_gen_enabled_attr = __ATTR(
 );
 
 static struct attribute *lru_gen_attrs[] = {
+	&lru_gen_min_ttl_unsatisfied_attr.attr,
 	&lru_gen_min_ttl_attr.attr,
 	&lru_gen_enabled_attr.attr,
 	NULL
