7d665ed049001f392828837fc8e557c0d1e3dae5
LuK1337 <priv.luk@gmail.com>
2022-06-22 09:54:21 +0200

    techpack: audio: Haxxx
    Change-Id: I2a9dbf7cf968281f98105bd7359d3e9fe20dba35

--- techpack/audio/dsp/q6afe.c
+++ techpack/audio/dsp/q6afe.c
@@ -1953,7 +1953,7 @@ static int q6afe_get_params(u16 port_id, struct mem_mapping_hdr *mem_hdr,
 #ifdef OPLUS_FEATURE_TFA98XX_VI_FEEDBACK
 int get_smartpa_id(void);
 #endif /*OPLUS_FEATURE_TFA98XX_VI_FEEDBACK*/
-static int set_smartpa_pm_status_apr(void *buf, int cmd_size)
+int set_smartpa_pm_status_apr(void *buf, int cmd_size)
 {
 	int ret;
 	int port = 0;
@@ -2028,9 +2028,8 @@ done:
 	}
 	return ret;
 }
-EXPORT_SYMBOL(set_smartpa_pm_status_apr);
 
-static int get_smartpa_pm_result_apr(void *buf, int cmd_size)
+int get_smartpa_pm_result_apr(void *buf, int cmd_size)
 {
 	int ret = 0;
 	int port = 0;
@@ -2086,7 +2085,6 @@ static int get_smartpa_pm_result_apr(void *buf, int cmd_size)
 done:
 	return ret;
 }
-EXPORT_SYMBOL(get_smartpa_pm_result_apr);
 #endif /* OPLUS_FEATURE_SMARTPA_PM */
 
 /*
