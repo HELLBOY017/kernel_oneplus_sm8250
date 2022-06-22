fdb984fda937143b9ea1cbd0d473790bc8066435
LuK1337 <priv.luk@gmail.com>
2022-06-22 16:06:15 +0200

    synaptics_tcm_oncell: Add support for SingleTap/SGESTRUE gestures
    Change-Id: I64f091320b987d3451c1fad03cc45def782a4abe

--- drivers/input/touchscreen/oplus_touchscreen/Synaptics/Syna_tcm_oncell/synaptics_tcm_oncell.c
+++ drivers/input/touchscreen/oplus_touchscreen/Synaptics/Syna_tcm_oncell/synaptics_tcm_oncell.c
@@ -3003,6 +3003,9 @@ static int syna_get_gesture_info(void *chip_data, struct gesture_info *gesture)
     case DTAP_DETECT:
         gesture->gesture_type = DouTap;
         break;
+    case STAP_DETECT:
+        gesture->gesture_type = SingleTap;
+        break;
     case CIRCLE_DETECT:
         gesture->gesture_type = Circle;
         if (touch_data->extra_gesture_info[2] == 0x10)
@@ -3031,6 +3034,9 @@ static int syna_get_gesture_info(void *chip_data, struct gesture_info *gesture)
         break;
     case M_UNICODE:
         gesture->gesture_type = Mgestrue;
+        break;
+    case S_UNICODE:
+        gesture->gesture_type = SGESTRUE;
         break;
     case W_UNICODE:
         gesture->gesture_type = Wgestrue;
