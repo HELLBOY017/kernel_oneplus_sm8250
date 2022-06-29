6071b71ed6a51453f8d15b50737b6474ab57170b
Dark-Matter7232 <kerneldeveloper7232@gmail.com>
2022-06-29 18:33:47 +0530

    mm: Import oplus memory management hacks
    - Add a separate swappiness (default 60) value to use if task isn't handled by kswapd.
    - Don't throttle tasks with OOM value < 0.
    - Increase zsmalloc's maximum page order.
    [cyberknight777]:
    - Reword Kconfig option.
    - Import with clean indents.
    - Be more descriptive in Kconfig option's description.
    
    Signed-off-by: Dark-Matter7232 <kerneldeveloper7232@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -135,6 +135,10 @@ static unsigned long zero_ul;
 static unsigned long one_ul = 1;
 static unsigned long long_max = LONG_MAX;
 static int one_hundred = 100;
+static int two_hundred = 200;
+#ifdef CONFIG_OPLUS_MM_HACKS
+extern int direct_vm_swappiness;
+#endif /* CONFIG_OPLUS_MM_HACKS */
 static int one_thousand = 1000;
 #ifdef CONFIG_PRINTK
 static int ten_thousand = 10000;
@@ -1758,6 +1762,17 @@ static struct ctl_table vm_table[] = {
 		.extra1		= &zero,
 		.extra2		= &one_hundred,
 	},
+#ifdef CONFIG_OPLUS_MM_HACKS
+	{
+	        .procname	= "direct_swappiness",
+		.data		= &direct_vm_swappiness,
+		.maxlen 	= sizeof(direct_vm_swappiness),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1 	= &zero,
+		.extra2 	= &two_hundred,
+	},
+#endif /* CONFIG_OPLUS_MM_HACKS */
 	{
 		.procname       = "want_old_faultaround_pte",
 		.data           = &want_old_faultaround_pte,
