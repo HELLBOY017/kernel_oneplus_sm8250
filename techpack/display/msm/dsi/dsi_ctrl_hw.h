004af65a4d3f31b2163f12e5593814ad21d5a237
Yuan Si <do4suki@gmail.com>
2022-07-23 15:31:05 +0800

    techpack: display: Import changes from oneplus/sm8250_s_12.1
    Signed-off-by: Yuan Si <do4suki@gmail.com>
    Change-Id: Ie89ec5dc5898f5511108fcc9b47933099297e86c

--- techpack/display/msm/dsi/dsi_ctrl_hw.h
+++ techpack/display/msm/dsi/dsi_ctrl_hw.h
@@ -20,6 +20,17 @@
 #define DSI_CTRL_HW_INFO(c, fmt, ...)	DRM_DEV_INFO(NULL, "[msm-dsi-info]: DSI_%d: "\
 		fmt, c ? c->index : -1,	##__VA_ARGS__)
 
+#ifdef OPLUS_BUG_STABILITY
+#undef DSI_CTRL_HW_ERR
+#include <soc/oplus/system/oplus_mm_kevent_fb.h>
+#define DSI_CTRL_HW_ERR(c, fmt, ...) \
+	do { \
+		DRM_DEV_ERROR(NULL, "[msm-dsi-error]: DSI_%d: "\
+			fmt, c ? c->index : -1,	##__VA_ARGS__); \
+		mm_fb_display_kevent_named(MM_FB_KEY_RATELIMIT_1H, fmt, ##__VA_ARGS__); \
+	} while(0)
+#endif /* OPLUS_BUG_STABILITY */
+
 /**
  * Modifier flag for command transmission. If this flag is set, command
  * information is programmed to hardware and transmission is not triggered.
