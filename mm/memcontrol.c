8c12d464e23865344de5f80686d4ff7692931726
John Galt <johngaltfirstrun@gmail.com>
2022-09-30 11:31:54 +0200

    mm/vmscan: bump swappiness

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -3551,9 +3551,6 @@ static int mem_cgroup_swappiness_write(struct cgroup_subsys_state *css,
 {
 	struct mem_cgroup *memcg = mem_cgroup_from_css(css);
 
-	if (val > 100)
-		return -EINVAL;
-
 	if (css->parent)
 		memcg->swappiness = val;
 	else
