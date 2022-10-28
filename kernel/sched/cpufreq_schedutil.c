639b5ff77092f1f96a03d1328aa4e7386d452f87
HELLBOY017 <abhaygill017@gmail.com>
2022-10-29 04:42:44 +0530

    sched: cpufreq_schedutil: Set DEFAULT_HISPEED_LOAD to 80 Matches oplus behaviour

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -593,7 +593,7 @@ static inline bool sugov_cpu_is_busy(struct sugov_cpu *sg_cpu) { return false; }
 #endif /* CONFIG_NO_HZ_COMMON */
 
 #define NL_RATIO 75
-#define DEFAULT_HISPEED_LOAD 90
+#define DEFAULT_HISPEED_LOAD 80
 #define DEFAULT_CPU0_RTG_BOOST_FREQ 1000000
 #define DEFAULT_CPU4_RTG_BOOST_FREQ 0
 #define DEFAULT_CPU7_RTG_BOOST_FREQ 0
