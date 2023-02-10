a29d537e41868da43149c373018202eef2f54ce6
jabashque <jabashque@gmail.com>
2023-02-10 10:07:55 -0800

    display: oplus: Don't skip pcc for doze modes
    Let's leave the color transformations applied while in Always-on Display
    mode.
    
    Change-Id: I7403ceb359d7f85b6e4169d2b8a0aab4373b8b64

--- techpack/display/oplus/oplus_onscreenfingerprint.c
+++ techpack/display/oplus/oplus_onscreenfingerprint.c
@@ -748,9 +748,7 @@ int sde_crtc_config_fingerprint_dim_layer(struct drm_crtc_state *crtc_state, int
 
 bool is_skip_pcc(struct drm_crtc *crtc)
 {
-	if (OPLUS_DISPLAY_POWER_DOZE_SUSPEND == get_oplus_display_power_status() ||
-	    OPLUS_DISPLAY_POWER_DOZE == get_oplus_display_power_status() ||
-	    sde_crtc_get_fingerprint_pressed(crtc->state))
+	if (sde_crtc_get_fingerprint_pressed(crtc->state))
 		return true;
 
 	return false;
