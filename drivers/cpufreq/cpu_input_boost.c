46a6ed9dfbbcdb26c469cbf16291958c65ba77b1
HELLBOY017 <abhaygill017@gmail.com>
2023-03-06 15:54:21 +0530

    cpu_input_boost: lcd_active_panel -> active_panel

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -400,7 +400,7 @@ static struct input_handler cpu_input_boost_input_handler = {
 	.id_table	= cpu_input_boost_ids
 };
 
-extern struct drm_panel *lcd_active_panel;
+extern struct drm_panel *active_panel;
 
 static int __init cpu_input_boost_init(void)
 {
@@ -424,14 +424,14 @@ static int __init cpu_input_boost_init(void)
 
 	b->msm_drm_notif.notifier_call = msm_drm_notifier_cb;
 	b->msm_drm_notif.priority = INT_MAX;
-	if (lcd_active_panel) {
-		ret = drm_panel_notifier_register(lcd_active_panel, &b->msm_drm_notif);
+	if (active_panel) {
+		ret = drm_panel_notifier_register(active_panel, &b->msm_drm_notif);
 		if (ret) {
 			pr_err("Unable to register fb_notifier: %d\n", ret);
 			goto unregister_handler;
 		}
 	} else {
-		pr_err("lcd_active_panel is null\n");
+		pr_err("active_panel is null\n");
 	}
 
 	thread = kthread_run_perf_critical(cpu_perf_mask, cpu_boost_thread, b, "cpu_boostd");
