8d2e7b1319da9ebc274d25ec085f7a36470bd3fc
infected_ <zzitoz@gmail.com>
2023-03-18 14:20:57 +0000

    anykernel: leave essentials only

--- anykernel/anykernel.sh
+++ anykernel/anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Meteoric Kernel by HELLBOY017
+kernel.string=Aurora Kernel by deepongi
 do.devicecheck=0
 do.modules=0
 do.systemless=0
