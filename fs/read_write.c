6688598f9ced99d9078f5a915a24bba29969421e
HELLBOY017 <abhaygill017@gmail.com>
2023-07-19 16:39:28 +0530

    drivers: Conditionally build KernelSU

--- fs/read_write.c
+++ fs/read_write.c
@@ -434,14 +434,18 @@ ssize_t kernel_read(struct file *file, void *buf, size_t count, loff_t *pos)
 }
 EXPORT_SYMBOL(kernel_read);
 
+#ifdef CONFIG_KERNELSU
 extern int ksu_handle_vfs_read(struct file **file_ptr, char __user **buf_ptr,
 			       size_t *count_ptr, loff_t **pos);
+#endif
 
 ssize_t vfs_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
 {
 	ssize_t ret;
 
+#ifdef CONFIG_KERNELSU
 	ksu_handle_vfs_read(&file, &buf, &count, &pos);
+#endif
 
 	if (!(file->f_mode & FMODE_READ))
 		return -EBADF;
