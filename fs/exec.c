40e8afa2be5f29861fca58acd775e1803ac6a5d2
HELLBOY017 <abhaygill017@gmail.com>
2023-07-19 16:39:28 +0530

    drivers: Conditionally build KernelSU

--- fs/exec.c
+++ fs/exec.c
@@ -1921,15 +1921,19 @@ out_ret:
 	return retval;
 }
 
+#ifdef CONFIG_KERNELSU
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 			       void *envp, int *flags);
+#endif
 
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
 			      int flags)
 {
+#ifdef CONFIG_KERNELSU
 	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+#endif
 	return __do_execve_file(fd, filename, argv, envp, flags, NULL);
 }
 
