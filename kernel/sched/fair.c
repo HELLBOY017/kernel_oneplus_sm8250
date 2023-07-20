34893c4fcd33fea61b8c125193d0f6052717c8b3
HELLBOY017 <abhaygill017@gmail.com>
2023-07-20 05:37:03 +0530

    Revert "sched: restrict iowait boost to tasks with prefer_idle"
    This reverts commit d866a877764711ae22ed256f5091684ac1cdebe8.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5524,7 +5524,7 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	 * utilization updates, so do it here explicitly with the IOWAIT flag
 	 * passed.
 	 */
-	if (p->in_iowait && prefer_idle)
+	if (p->in_iowait)
 		cpufreq_update_util(rq, SCHED_CPUFREQ_IOWAIT);
 
 	for_each_sched_entity(se) {
