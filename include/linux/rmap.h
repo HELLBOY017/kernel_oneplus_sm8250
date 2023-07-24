873223637935ba5208569617f4b79cd8e5f27031
HELLBOY017 <abhaygill017@gmail.com>
2023-07-24 16:47:51 +0530

    mm: Add back oplus damon

--- include/linux/rmap.h
+++ include/linux/rmap.h
@@ -16,6 +16,7 @@ extern int isolate_lru_page(struct page *page);
 extern void putback_lru_page(struct page *page);
 extern unsigned long reclaim_pages_from_list(struct list_head *page_list,
 					     struct vm_area_struct *vma);
+extern unsigned long reclaim_pages(struct list_head *page_list);
 
 /*
  * The anon_vma heads a list of private "related" vmas, to scan if
